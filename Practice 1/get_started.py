a8584f2deb52b26b66b17922f2891a676b6a57c2
Rassul <rasulkanash@gmail.com>
2026-01-30 13:47:22 +0500

    get_started file is done

--- Practice 1/get_started.py
+++ Practice 1/get_started.py
@@ -2,3 +2,9 @@ print("This is a test for a version")
 import sys
 
 print(sys.version)
+print("This topic contains a lot of information about " \
+"how to check what is a python version is installed")
+print("The version is,", sys.version)
+print("The version is,", sys.version)
+print("The version is,", sys.version)
+print("The version is,", sys.version)

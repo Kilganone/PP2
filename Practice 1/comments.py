acbd7435e1cd1531cf13d0196768c3513d91e8bd
Rassul <rasulkanash@gmail.com>
2026-01-30 22:53:38 +0500

    commets file added

---
+++ Practice 1/comments.py
@@ -0,0 +1,29 @@
+#this is a top comment
+print("Hello there")
+
+print(25+5) #this is a side comment
+
+#previously were used one line comments
+"""this is a multi line
+comments
+it can be written among some 
+lines
+and they will not cause
+any mistakes
+"""
+#also comments are usefull for prevention code executing
+#print("Hello, Garry")
+print("Hello, Michael")
+#only "Hello, Michael" will be executed, because print("Hello, Garry") is a comment
+"""awdawda
+awd
+wdwd"""
+"""More 
+example"""
+"""Nice
+work"""
+
+
+"""The final
+multi line
+comment"""

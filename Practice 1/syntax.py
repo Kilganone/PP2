c5af72aeff4db6dcf91a09335d98c384a1b8f7da
Rassul <rasulkanash@gmail.com>
2026-01-30 22:28:08 +0500

    added comments into syntax.py

--- Practice 1/syntax.py
+++ Practice 1/syntax.py
@@ -1,3 +1,4 @@
+#examples of different indendations
 if 5>2:
     print("5 is greater than 2")
 if 7>3:
@@ -13,12 +14,16 @@ if x>y:
       print(x,comp,y)
 else:
        print(y,comp,x)
+#examples of the statements
 print("x=",x); print("y=",y); print("x+y=", x+y)
 print("Hello World!")
 print("Have a good day.")
 print("Learning Python is fun!") 
+#some print statements can be written in the same line
 print("Hello World!111");print("Have a good day.111");print("Learning Python is fun!111") 
 print("Hi");print("How are u");print("What u do?")
+#more examples
 print("Whatsupp");print("good");print("nothing")
 print("indentation");print("are really important"); print("in python!")
+#the last example
 print("also statements can be written throught ';' sign")

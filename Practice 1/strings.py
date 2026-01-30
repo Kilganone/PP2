290d5eeed3bc82f432084a747e979b4664fc6239
Rassul <rasulkanash@gmail.com>
2026-01-31 00:24:27 +0500

    strings file added

---
+++ Practice 1/strings.py
@@ -0,0 +1,94 @@
+print("Python is cool")
+print('I love coding')
+print("She said, 'Hi!'")
+print('He said, "Hello!"')
+print("12345")
+print()
+
+a = "Apple"
+b = 'Banana'
+c = "123"
+d = "Python 3"
+e = 'Good morning'
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print()
+
+text1 = """This is line one
+This is line two
+This is line three"""
+print(text1)
+
+text2 = '''First line
+Second line
+Third line'''
+print(text2)
+
+text3 = """A
+B
+C"""
+print(text3)
+
+text4 = '''Hello
+World'''
+print(text4)
+
+text5 = """Python
+is
+awesome"""
+print(text5)
+print()
+
+word = "Programming"
+
+print(word[0])
+print(word[3])
+print(word[5])
+print(word[7])
+print(word[-1])
+print()
+
+for letter in "apple":
+    print(letter)
+
+for ch in "code":
+    print(ch)
+
+for x in "12345":
+    print(x)
+
+for y in "Hi!":
+    print(y)
+
+for z in "Python":
+    print(z)
+
+print()
+
+print(len("Computer"))
+print(len("Science"))
+print(len("123456789"))
+print(len("Hi"))
+print(len("Python Programming"))
+print()
+
+text = "Learning Python is fun"
+
+print("Python" in text)
+print("Java" in text)
+print("fun" in text)
+print("Learning" in text)
+print("easy" in text)
+print()
+
+text2 = "I like playing football"
+
+print("basketball" not in text2)
+print("football" not in text2)
+print("tennis" not in text2)
+print("I" not in text2)
+print("playing" not in text2)

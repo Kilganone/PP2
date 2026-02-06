e93de2180004ca3dc24d01d20ee4a27b2fa1fe2d
Rassul <rasulkanash@gmail.com>
2026-02-07 01:06:46 +0500

    while_break added

---
+++ Practice 2/loops/while_break.py
@@ -0,0 +1,34 @@
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+x = 0
+while x < 10:
+  print(x)
+  if x == 5:
+    break
+  x += 1
+
+count = 1
+while count <= 5:
+  if count == 4:
+    break
+  print(count)
+  count += 1
+
+i = 10
+while i > 0:
+  print(i)
+  if i == 7:
+    break
+  i -= 1
+
+x = 1
+while True:
+  print(x)
+  if x == 3:
+    break
+  x += 1

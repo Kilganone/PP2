f2dd638612585a1d0c645fef1293a45ac1bf04bd
Rassul <rasulkanash@gmail.com>
2026-02-07 00:44:18 +0500

    boolean comparison added

---
+++ Practice 2/boolean/boolean_comparison.py
@@ -0,0 +1,25 @@
+x = 10
+y = 5
+print(x > y)
+print(x < y)
+print(x == y)
+print(x != y)
+print(x >= 10)
+print(x <= 9)
+
+a = 7
+b = 7
+print(a == b)
+print(a != b)
+
+age = 18
+print(age >= 18)
+print(age < 13)
+
+temperature = 98.6
+print(temperature > 100.4)
+print(temperature <= 98.6)
+
+score = 85
+print(score >= 90)
+print(score < 70)

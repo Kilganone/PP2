9ae448993b6beb79a65daad64d0a6616f375c766
Rassul <rasulkanash@gmail.com>
2026-02-07 01:07:39 +0500

    while_continue added

---
+++ Practice 2/loops/while_continue.py
@@ -0,0 +1,35 @@
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+x = 0
+while x < 5:
+  x += 1
+  if x == 2:
+    continue
+  print(x)
+
+count = 0
+while count < 4:
+  count += 1
+  if count % 2 == 0:
+    continue
+  print(count)
+
+i = 1
+while i <= 5:
+  if i == 4:
+    i += 1
+    continue
+  print(i)
+  i += 1
+
+x = 5
+while x > 0:
+  x -= 1
+  if x == 2:
+    continue
+  print(x)

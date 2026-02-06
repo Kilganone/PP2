5f9920a7dd5c897ec5e55b05f062bf5a93fcaa89
Rassul <rasulkanash@gmail.com>
2026-02-07 00:47:31 +0500

    if_statements added

---
+++ Practice 2/if-else/if_statements.py
@@ -0,0 +1,41 @@
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+number = 15
+if number > 0:
+  print("The number is positive")
+
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+temperature = 75
+if temperature > 70:
+  print("It's warm outside")
+
+score = 85
+if score >= 90:
+  print("Grade: A")
+
+has_items = [1, 2, 3]
+if has_items:
+  print("Cart has items")
+
+is_empty = []
+if not is_empty:
+  print("List is empty")
+
+password = "secret123"
+if len(password) >= 8:
+  print("Password length OK")
+
+balance = 100
+if balance > 0:
+  print("Account funded")

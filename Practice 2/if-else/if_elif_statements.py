59caf6f049b7d074b0a384f7581be782f1fa6c58
Rassul <rasulkanash@gmail.com>
2026-02-07 00:51:04 +0500

    if_elif_statements added

---
+++ Practice 2/if-else/if_elif_statements.py
@@ -0,0 +1,64 @@
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+score = 75
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+else:
+  print("Grade: F")
+
+age = 25
+if age < 13:
+  print("Child")
+elif age < 20:
+  print("Teenager")
+elif age < 65:
+  print("Adult")
+else:
+  print("Senior")
+
+day = 3
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+else:
+  print("Sunday")
+
+temperature = 22
+if temperature > 30:
+  print("Hot")
+elif temperature > 20:
+  print("Warm")
+elif temperature > 10:
+  print("Cool")
+else:
+  print("Cold")
+
+x = 10
+if x > 100:
+  print("Very large")
+elif x > 50:
+  print("Large")
+elif x > 10:
+  print("Medium")
+else:
+  print("Small")

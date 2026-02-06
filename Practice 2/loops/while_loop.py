69ec08d8250d54f58284e40bdf8e2af187031381
Rassul <rasulkanash@gmail.com>
2026-02-07 01:05:53 +0500

    while_loop added

---
+++ Practice 2/loops/while_loop.py
@@ -0,0 +1,26 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+count = 0
+while count < 3:
+  print("Hello")
+  count += 1
+
+x = 10
+while x > 5:
+  print(x)
+  x -= 1
+
+num = 0
+while num <= 4:
+  print(num * 2)
+  num += 1
+
+i = 1
+while i < 4:
+  print(i)
+  i += 1
+else:
+  print("Loop finished")

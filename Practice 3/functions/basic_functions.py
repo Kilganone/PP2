30b3a2ce08ecb2ece57babbff6145b0cb6788cf7
Rassul <rasulkanash@gmail.com>
2026-02-20 19:23:39 +0500

    basic_functions added

---
+++ Practice 3/functions/basic_functions.py
@@ -0,0 +1,144 @@
+def greet():
+    print("Hello!")
+
+def say_bye():
+    print("Goodbye!")
+
+def show_number():
+    print(10)
+
+def show_message():
+    print("Python is fun!")
+
+def welcome():
+    print("Welcome to the program!")
+
+greet()
+say_bye()
+show_number()
+show_message()
+welcome()
+
+greet()
+greet()
+greet()
+
+say_bye()
+say_bye()
+
+def calculate_sum():
+    print(5 + 3)
+
+def _private_function():
+    print("Private")
+
+def myFunction2():
+    print("Function 2")
+
+def convert_value():
+    print("Converted")
+
+def check_status():
+    print("Status OK")
+
+calculate_sum()
+_private_function()
+myFunction2()
+convert_value()
+check_status()
+
+temp1 = 32
+print((temp1 - 32) * 5 / 9)
+
+temp2 = 50
+print((temp2 - 32) * 5 / 9)
+
+temp3 = 77
+print((temp3 - 32) * 5 / 9)
+
+temp4 = 95
+print((temp4 - 32) * 5 / 9)
+
+temp5 = 120
+print((temp5 - 32) * 5 / 9)
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(32))
+print(fahrenheit_to_celsius(50))
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(120))
+
+def get_greeting():
+    return "Hello!"
+
+def get_number():
+    return 100
+
+def get_pi():
+    return 3.14
+
+def get_status():
+    return "Success"
+
+def get_boolean():
+    return True
+
+print(get_greeting())
+print(get_number())
+print(get_pi())
+print(get_status())
+print(get_boolean())
+
+message = get_greeting()
+print(message)
+
+number = get_number()
+print(number)
+
+pi_value = get_pi()
+print(pi_value)
+
+status = get_status()
+print(status)
+
+boolean_value = get_boolean()
+print(boolean_value)
+
+def no_return1():
+    print("Test 1")
+
+def no_return2():
+    print("Test 2")
+
+def no_return3():
+    x = 5 + 5
+
+def no_return4():
+    pass
+
+def no_return5():
+    print("Done")
+
+print(no_return1())
+print(no_return2())
+print(no_return3())
+print(no_return4())
+print(no_return5())
+
+def empty_function1():
+    pass
+
+def empty_function2():
+    pass
+
+def empty_function3():
+    pass
+
+def empty_function4():
+    pass
+
+def empty_function5():
+    pass

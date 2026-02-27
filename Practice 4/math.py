c4aac88697674579c875c32b5f768eacce9a44fe
Rassul <rasulkanash@gmail.com>
2026-02-27 23:29:00 +0500

    math added

---
+++ Practice 4/math.py
@@ -0,0 +1,49 @@
+import math
+
+print(min(5, 10, 25))
+print(min(100, 50, 75))
+print(min(-5, -10, -1))
+print(min(3.5, 3.1, 3.9))
+print(min(10, 5, 20, 1, 50))
+
+print(max(5, 10, 25))
+print(max(100, 50, 75))
+print(max(-5, -10, -1))
+print(max(3.5, 3.1, 3.9))
+print(max(10, 5, 20, 1, 50))
+
+print(abs(-7.25))
+print(abs(-10))
+print(abs(5))
+print(abs(0))
+print(abs(3.14 - 5))
+
+print(pow(4, 3))
+print(pow(2, 5))
+print(pow(10, 2))
+print(pow(5, 0))
+print(pow(2.5, 2))
+
+print(math.sqrt(64))
+print(math.sqrt(100))
+print(math.sqrt(2))
+print(math.sqrt(0))
+print(math.sqrt(1))
+
+print(math.ceil(1.4))
+print(math.ceil(1.1))
+print(math.ceil(5.9))
+print(math.ceil(-1.4))
+print(math.ceil(0.1))
+
+print(math.floor(1.4))
+print(math.floor(1.9))
+print(math.floor(5.1))
+print(math.floor(-1.4))
+print(math.floor(0.9))
+
+print(math.pi)
+print(round(math.pi, 2))
+print(math.pi * 2)
+print(math.pi * (5 ** 2))
+print(2 * math.pi * 3)
